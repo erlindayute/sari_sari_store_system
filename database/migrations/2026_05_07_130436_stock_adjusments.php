2151e4a373f293cf550eb9f9edb7872fd8f3a5ac
erlinda yute <erlindayutesanco74@gmail.com>
2026-05-11 16:31:06 +0200

    refactore routes

---
+++ database/migrations/2026_05_07_130436_stock_adjusments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_adjustments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity_before');
+            $table->integer('quantity_after');
+            $table->integer('adjustment');
+            $table->enum('type', ['addition', 'remove', 'set', 'sale']);
+            $table->string('reason')->nullable();
+            $table->timestamp('adjusted_at')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_adjustments');
+    }
+};
