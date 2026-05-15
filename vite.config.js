194c331e2ebeeeba4749b2ee8eb93ad796f301ba
erlinda yute <erlindayutesanco74@gmail.com>
2026-05-15 16:36:02 +0200

    added register.css and register.jc

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/register.css',
+                'resources/js/app.js',
+                'resources/js/register.js',
+            ],
+
             refresh: true,
         }),
         tailwindcss(),
